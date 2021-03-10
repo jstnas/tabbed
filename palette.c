6210c337a3f97ed6e3fae577aa7a752d68bf2bb9
Justinas Grigas <jstn_as@protonmail.com>
2021-03-10 11:38:18 +0000

    Changed the palette

--- palette.c
+++ palette.c
@@ -1,6 +1,6 @@
-static const char* normbgcolor  = "#0f0f0f";
-static const char* normfgcolor  = "#cfcfcf";
-static const char* selbgcolor   = "#ff3f7f";
-static const char* selfgcolor   = "#ffffff";
-static const char* urgbgcolor   = "#ffff7f";
-static const char* urgfgcolor   = "#ffffff";
+static const char* normbgcolor  = "#1f1f2f";
+static const char* normfgcolor  = "#8f8f9f";
+static const char* selbgcolor   = "#9f4f5f";
+static const char* selfgcolor   = "#efefff";
+static const char* urgbgcolor   = "#9f7f4f";
+static const char* urgfgcolor   = "#efefff";
