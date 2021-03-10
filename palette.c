4fec2d703ac6100233b5928323b714994c0e65fc
Justinas Grigas <jstn_as@protonmail.com>
2021-03-10 18:41:57 +0000

    Yellow accent

--- palette.c
+++ palette.c
@@ -1,6 +1,6 @@
 static const char* normbgcolor  = "#1f1f2f";
 static const char* normfgcolor  = "#8f8f9f";
-static const char* selbgcolor   = "#9f4f5f";
+static const char* selbgcolor   = "#9f7f4f";
 static const char* selfgcolor   = "#efefff";
-static const char* urgbgcolor   = "#9f7f4f";
+static const char* urgbgcolor   = "#9f4f6f";
 static const char* urgfgcolor   = "#efefff";
