df18227f7a392fab6234943aa4c2a16b73f60022
Justinas Grigas <jstn_as@protonmail.com>
2021-04-19 01:56:50 +0100

    Better colours

--- palette.c
+++ palette.c
@@ -1,6 +1,6 @@
-static const char* normbgcolor  = "#1f1f2f";
-static const char* normfgcolor  = "#8f8f9f";
-static const char* selbgcolor   = "#9f7f4f";
-static const char* selfgcolor   = "#efefff";
+static const char* normbgcolor  = "#0f1f2f";
+static const char* normfgcolor  = "#6f7f8f";
+static const char* selbgcolor   = "#9f6f4f";
+static const char* selfgcolor   = "#cfdfef";
 static const char* urgbgcolor   = "#9f4f6f";
-static const char* urgfgcolor   = "#efefff";
+static const char* urgfgcolor   = "#cfdfef";
