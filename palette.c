c97ef46aa72ecbc2a5dca3fc985467bfe04acf40
Justinas Grigas <jstn_as@protonmail.com>
2021-05-30 00:52:24 +0000

    palette updated to new colors

--- palette.c
+++ palette.c
@@ -1,6 +1,6 @@
 static const char* normbgcolor  = "#0f1f2f";
 static const char* normfgcolor  = "#6f7f8f";
-static const char* selbgcolor   = "#9f6f4f";
+static const char* selbgcolor   = "#7f4f2f";
 static const char* selfgcolor   = "#cfdfef";
-static const char* urgbgcolor   = "#9f4f6f";
+static const char* urgbgcolor   = "#7f2f4f";
 static const char* urgfgcolor   = "#cfdfef";
